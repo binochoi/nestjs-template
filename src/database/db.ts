43f56c4bdfaf12ff1216a2d51e17b2a55f608800
bino <bonoself@gmail.com>
2024-01-07 13:37:09 +0900

    export db

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,12 +4,12 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new Pool({
+export const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
-const db = drizzle(client);
+const db = drizzle(pool);
 export default db;
