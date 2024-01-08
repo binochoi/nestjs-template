0f13e276ed9d5ace9f629ffc00c23cd575f04c01
bino <bonoself@gmail.com>
2024-01-08 16:54:44 +0900

    feat: add global folder

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,15 +1,19 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
+import * as schema from './schema';
 
 dotenv.config();
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
+const client = new Pool({
+  host: '34.22.67.164',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: 'postgres',
+  password: 'qbt8w7!%^23h',
+  database: 'yeo-tiger',
 });
-const db = drizzle(pool);
+const db = drizzle(client, { schema });
+export {
+  client,
+};
 export default db;
